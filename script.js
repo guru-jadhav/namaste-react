e06e5170ab9cde973b2a0d8ed645fea5faba0e54
guru-jadhav <jguru4710@gmail.com>
2024-10-29 00:11:11 +0530

    class 3

--- script.js
+++ script.js
@@ -1,61 +1,77 @@
-import React from "react"
-import ReactDOM from "react-dom/client"
+import React from "react";
+import ReactDOM from "react-dom/client";
+import React, { Component } from "react";
 
-// React.createElement("elementName", {"attributs and props(like id, class, key, etc)"}, children's of the tag(we can create multiple childs of this element we are creating))
+// React Element using React.createElement
+const heading = React.createElement("div", {}, [
+  React.createElement("h1", {}, "Namaste from React"),
+  React.createElement("h1", {}, "Hello from React"),
+]);
+// console.log(heading);
 
-const greetingReact = React.createElement(
-    "div",
-    { id: 1, className: "greet" },
-    [
-        // keys
-        // when we use key prop, this is a internal mechanism (internal React prop)
-        // used for performance optimization and reconciliation of lists. it will not be displayed in the HTML output
-        React.createElement("h1", {key:1}, "Hello from React"),
-        React.createElement("h1", {key:2}, "Namaste from React")
-    ]
-    // React.createElement("h1", null, "Hello from React"),
-    // React.createElement("h1", null, "Namaste from React")
-    // when we create multiple childrens of a element they are stored as Array in 
-    // the children property of the object(element we created)
+// React Element using JSX
+const headingJSX = (
+  <div>
+    <h1 id="1" className="heading1" tabIndex={2}>
+      Namaste from React
+    </h1>
+    <h1 id="2" className="heading2" tabIndex={3}>
+      Hello from React
+    </h1>
+  </div>
 );
-
-// JSX(javascript XML) allows us to write HTML-like code within our javascript file.
-// It makes it easier to create and visualize the structure of your React components. 
-// JSX(javascript XML) is syntactic sugar for React.createElement, 
-// The conversion from JSX to React.createElement is done by js compiler like Babel
-// Babel is responsible for converting JSX syntax into React.createElement calls during the build process.
-// under the hood it uses React.createElement to actually create the React elements
-// Most React applications are set up using build tools like Webpack, Parcel, or Vite. 
-// These tools integrate Babel into the build process, allowing you to write JSX in your source files, 
-// and automatically transforming it during the build.
+// console.log(headingJSX);
 
 
-// when we create a new element using React.createElement it will return us a object(JS object) and this object we get in return is a React Element,
-// which will be converted to HTML that browser understand when we render it using .render method
-console.log(typeof greetingReact);
-console.log(greetingReact);
+
+// React components are of two types:
+// 1 - Class Based components - OLD
+// 2 - Functional components - NEW
+
+// CLASS COMPONENT
+// This is class based component
+// class based component extends React.Component class
+// we have to use render() method that returns the component's UI
+class Myclass extends Component {
+  render() {
+    return <h1>Hello from class component !</h1>;
+  }
+}
 
 
+// FUNCTION COMPONENT
+// This is function based component
+const HeadingFunction = () => {
+  return <h1>Hello from function component</h1>;
+};
+
+// This is function based component
+function HeadingFunction2(props) {
+  return <h1>Hello from {props.name} function</h1>;
+}
+
+// This is function based component
+// what is functional component: It is a normal Javascript function which return some peace of JSX code or ReactElement
+// It can take props as its parameter(input)
+// Function based component should start with capital letter in it's function name
+
+function App() {
+  let name = "Guru";
+  return (
+    <div id="container">
+      <HeadingFunction />
+      {/* inside this curly braces we can write any javascript code and it will be executed when this component will be rendered */}
+      {HeadingFunction()}
+      {/* As functional components are normal javascript function we can directly call this function instead of rendering them with JSX tags like <HeadingFunction />  */}
+      <HeadingFunction2 name={name} />
+      <h1>Hello from app component</h1>
+      <Myclass />
+    </div>
+  );
+}
+
+// Component composition: Displaying a component inside another component is called as component compostion
+// This is helpfull for building complex UI
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(greetingReact);
-// when we are rendering this greetingReact the .render(.render which is the part of ReactDOM) method will convert this object(which we got it from React.createElement)
-// to a actual HTML elements and render them on the screen
-// ReactDOM is the one that converts the Virtual DOM objects (React elements) into 
-// actual HTML tags and renders them on the screen.
-// ReactDOM enables React to create actual HTML elements from react Elements(which we got from React.createElement)
-// When we do root.render and if there was something already present in that root(any childeren), then those
-// children will be removed(replaced)
-// When calling root.render(greetingReact), if there are any existing child elements within the root container, 
-// React will unmount those elements and "replace" them with the new React element. This process involves 
-// a reconciliation step, where React compares the current Virtual DOM representation with the new one and updates the DOM accordingly.
-
-
-
-
-
-// React can be applied to small portion of our page (and the rest of the page will be working as it is(that's why react is called a library))
-// React can be integrated into specific parts of a webpage, allowing those sections to leverage React's features and 
-// capabilities while the rest of the page remains unaffected. This flexibility is one of the reasons React is 
-// considered a library rather than a full-fledged framework. It can coexist with other JavaScript libraries and frameworks, 
-// enabling developers to enhance existing applications incrementally without having to rewrite the entire codebase.
+root.render(<App></App>);
